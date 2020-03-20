92260c98c09cb9e854ea8e38340f9ad6e86505f8
Andreas Meier <andreas.meier@unibas.ch>
2020-03-20 17:02:52 +0100

    Draft main list input

--- src/main/java/com/example/poschtizettel/SecondFragment.kt
+++ src/main/java/com/example/poschtizettel/SecondFragment.kt
@@ -1,12 +1,13 @@
 package com.example.poschtizettel
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.textfield.TextInputEditText
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -16,6 +17,8 @@ class SecondFragment : Fragment() {
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
+
+
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_second, container, false)
@@ -27,5 +30,22 @@ class SecondFragment : Fragment() {
         view.findViewById<Button>(R.id.button_second).setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
+        view.findViewById<Button>(R.id.button_add_list).setOnClickListener {
+            val asdf = view.findViewById<TextInputEditText>(R.id.textInputEdit_addList)
+            val listName = asdf.text.toString()
+            val newList = ShoppingList(listName)
+
+
+            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        }
+
+
+
+
+    }
+
+    interface addShoppingListInterface{
+        fun addShoppingList(newList: ShoppingList)
+
     }
 }
