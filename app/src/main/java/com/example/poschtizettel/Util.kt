d4d5c50cb14a7541ccee4c08b9ef9fdd2de0248c
Andreas Meier <andreas.meier@stud.unibas.ch>
2020-04-16 09:51:37 +0200

    Fix wrong root directory

---
+++ app/src/main/java/com/example/poschtizettel/Util.kt
@@ -0,0 +1,31 @@
+package com.example.poschtizettel
+
+import android.text.Html
+import android.text.Spanned
+import com.example.poschtizettel.database.ShoppingItems
+import com.example.poschtizettel.database.ShoppingList
+
+
+fun formatShoppingList(list: ShoppingList){
+
+}
+
+fun formatShoppingLists(lists: List<ShoppingList>) : Spanned{
+    val sb = StringBuilder()
+    sb.apply { append("<h3>Shopping Lists</h3>")
+        lists.forEach{
+            append("<p>${it.name}")
+        }}
+
+    return Html.fromHtml(sb.toString())
+
+
+}
+
+fun formatShoppingItem(item: ShoppingItems){
+
+}
+
+fun formatShoppingItem(items: List<ShoppingItems>){
+
+}
