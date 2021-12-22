e0ac7d0e0f75ce5337fd57a5ed45cc2a2875a245
Andreas Meier <andreas.meier@stud.unibas.ch>
2021-12-22 09:45:51 +0100

    Save and prefill settings

--- app/src/main/java/com/example/poschtizettel/SyncFragment.kt
+++ app/src/main/java/com/example/poschtizettel/SyncFragment.kt
@@ -1,15 +1,31 @@
 package com.example.poschtizettel
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.lifecycle.ViewModelProvider
+import com.example.poschtizettel.database.PoschtiDatabase
+import com.google.android.material.textfield.TextInputEditText
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+private const val TOKEN = "token"
+private const val URL = "url"
+private const val USERNAME = "username"
+private const val GET_URL = ""
+private const val NUKE_URL = ""
+private const val REMOTE_URL = ""
+
+
 
 /**
  * A simple [Fragment] subclass.
@@ -20,6 +36,7 @@ class SyncFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var viewModel: ListsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,8 +50,44 @@ class SyncFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val application = requireNotNull(this.activity).application
+
+        val dataSource = PoschtiDatabase.getInstance(application).poschtiDatabaseDao
+
+        val viewModelFactory = ListsViewModelFactory(dataSource, application)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(ListsViewModel::class.java)
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sync, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        val prefs = activity?.getPreferences(Context.MODE_PRIVATE)
+        if (prefs != null) {
+            prefill(view, prefs)
+        }
+
+
+
+        view.findViewById<Button>(R.id.button_apply).setOnClickListener {
+            val name = view.findViewById<AppCompatEditText>(R.id.editTextTextUsername).text.toString()
+            val url = view.findViewById<AppCompatEditText>(R.id.editTextTextURL).text.toString()
+            val token = view.findViewById<AppCompatEditText>(R.id.editTextTextToken).text.toString()
+
+            with(prefs!!.edit()){
+                putString(USERNAME, name)
+                putString(TOKEN, token)
+                putString(URL, url)
+
+                apply()
+
+            }
+            Log.i("ApplySettings", "applySettings: ${name}. ${url}, ${token}")
+
+        }
+
     }
 
     companion object {
@@ -55,5 +108,25 @@ class SyncFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    fun prefill(view: View, prefs: SharedPreferences){
+
+
+        val nameField = view.findViewById<AppCompatEditText>(R.id.editTextTextUsername)
+        nameField.setText(prefs.getString(USERNAME, "Username"))
+        val tokenField = view.findViewById<AppCompatEditText>(R.id.editTextTextToken)
+        tokenField.setText(prefs.getString(TOKEN, "Token"))
+        val urlField = view.findViewById<AppCompatEditText>(R.id.editTextTextURL)
+        urlField.setText(prefs.getString(URL, "URL"))
+
+
+    }
+
+
+    fun applySettings(){
+
+
+
     }
 }
